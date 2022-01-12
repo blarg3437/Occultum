0a2a0e5e614a7130a72f0f915596070de72919ef
Nick Brown <nick00@vt.edu>
2022-01-12 11:36:30 -0500

    HI

--- Occult/World/Components/AnimationComponent.cs
+++ Occult/World/Components/AnimationComponent.cs
@@ -19,8 +19,12 @@ namespace Occult.World.Components
 
 
         //Actual animation stuff
+        private Dictionary<DIRECTION, int[]> framesDictionary;
         Timer timer;
+        private DIRECTION previousDirection;
         DIRECTION direction;
+        int currentFrame;
+
 
         public AnimationComponent(float timeBetweenFrames)
         {
@@ -34,9 +38,39 @@ namespace Occult.World.Components
 
         public void update(GameTime gameTime)
         {
-            
+            if(timer.tick())
             //when its time use this
             OnSourceUpdate(sourceRectangle);
+        }
+
+        public void onPositionChangeHandler(DIRECTION dir)
+        {
+            if (previousDirectiond is null) previousDirection = dir;
+            previousDirection = direction;
+            direction = dir;
+        }
+
+        public void addFrameArray(DIRECTION dir, int[] arr)
+        {
+            framesDictionary.Add(dir, arr);
+        }
+
+        private void advanceCurrentFrame()
+        {
+            //I need to check if the direction has changed, and if it has- set the frame to the 
+            //first frame designated for that direction animation set.
+            if(direction != previousDirection)
+            {
+                //direction has been updated, so set the animation fram eto the first in the direction set.
+                currentFrame = framesDictionary[direction][0];
+                previousDirection = direction;
+                return;
+            }
+            //if that is not the case-proceed as normal. 
+            if (currentFrame == framesDictionary[direction].LastOrDefault)
+            { currentFrame = framesDictionary[direction][0]; return; }
+
+            currentFrame++;
         }
     }
 }
